046c687872f0e06e02e85bf6890134495d85e5db
Ky Quoc Viet <kq.viet@gmail.com>
2016-04-12 00:07:17 +0700

    Fetch images + socket.io integration

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# chotot-assignment
-ChoTot's Assignment
+## Cho Tot's Assignment
+
+***TODO***
+- Position images
+- Drag & Drop / Update to server
+- Slice if overflow
+- Animation!?
+
+***DONE***
+- Fetch Images on Server
+- Send to client by Socket.io
---
+++ app.js
@@ -0,0 +1,71 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+var cheerio = require('cheerio');
+var request = require('request');
+var object_assign = require('object-assign');
+
+app.listen(8080);
+
+function handler(req, res) {
+    fs.readFile(__dirname + '/public/index.html',
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+
+            res.writeHead(200);
+            res.end(data);
+        });
+}
+
+var FETCH_INTERVAL = 1000;
+var HOST = "http://www.chotot.vn";
+var PATH = "/tp-ho-chi-minh/mua-ban/#";
+var URL = HOST + PATH;
+var imgs = {};
+var i = 0;
+
+io.on('connection', function (socket) {
+    socket.emit('imageDownloaded', imgs);
+    var fetchDone = true;
+    setInterval(function () {
+        if (!fetchDone) return;
+        fetchDone = false;
+        request(URL, function (err, resp, body) {
+            $ = cheerio.load(body);
+            var rows = $('img.thumbnail');
+            var newImgs = {};
+            var changed = false;
+            for (var key in rows) {
+                var row = rows[key];
+                if (row && row.attribs) {
+                    var attr = row.attribs.src;//['data-original'];
+                    if (attr && !imgs[attr]) {
+                        newImgs[attr] = {
+                            position: i++
+                        };
+                        changed = true;
+                    }
+                    attr = row.attribs['data-original'];
+                    if (attr && !imgs[attr]) {
+                        newImgs[attr] = {
+                            position: i++
+                        };
+                        changed = true;
+                    }
+                }
+            }
+            if (changed === true) {
+                console.log('changed', newImgs);
+                imgs = object_assign({}, imgs, newImgs);
+                socket.emit('imageDownloaded', newImgs);
+            }
+            fetchDone = true;
+        });
+    }, FETCH_INTERVAL);
+    // socket.on('my other event', function (data) {
+    //     console.log(data);
+    // });
+});
